da880bb3695d15df160c16ebc4257fe14e7e0ba9
AnJung <jimmmy0719191@gmail.com>
2019-08-11 00:31:13 +0800

    update: add wrong_word to main

--- app/main.py
+++ app/main.py
@@ -2,25 +2,29 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 import json
 
+from modules.wrong_word.wrong_word import wrong_word_checker
 from modules.zhuyin.zhuyin import handle_tg_message as zhuyin_handle
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 with open('config.json') as config_file:
     config = json.load(config_file)
 
+
 def message_handler(bot, update):
     message = update.message.text
     zhuyin_reply = zhuyin_handle(message, update.message.from_user.id)
+    wrong_word_reply = wrong_word_checker(message)
 
-    if zhuyin_reply != None:
+    if zhuyin_reply is not None:
         update.message.reply_markdown(zhuyin_reply)
+    elif wrong_word_reply is not None:
+        update.message.reply_text(wrong_word_reply)
 
-    
 
 updater = Updater(config['bot_token'])
 updater.dispatcher.add_handler(MessageHandler(Filters.text, message_handler))
 
-updater.start_polling()  
+updater.start_polling()
 print('bot started')
 updater.idle()
