a41c9cf2d410a8eb71b5aeed6a4cdf5cff18054a
borm22244 <bryan2224431@gmail.com>
2019-08-11 01:29:31 +0800

    Add yan.py

--- app/modules/Yan/yan.py
+++ app/modules/Yan/yan.py
@@ -2,13 +2,20 @@ import json
 import random, time
 from telegram.ext import InlineQueryHandler
 from telegram import InlineQueryResultArticle,InputTextMessageContent
+import logging
 import uuid
-YanDataPath='app/modules/Yan/yan.json'
-file=open(YanDataPath,encoding='utf-8')
-info=file.read()
+YanDataPath='yan.json'
+f=open(YanDataPath,encoding='utf-8')
+info=f.read()
 Data=dict(json.loads(info))['list']
+tags='['
+for item in Data:
+    tags=tags+"["+item["tag"]+"]"+'\n'
+    
 def post_yan(bot, update):
     query = update.inline_query.query
+    if len(query) == 0:
+        return
     results=list()
     for i in Data:
         if query in i['tag']:
@@ -22,7 +29,7 @@ def post_yan(bot, update):
                 )
     print(len(results))
     update.inline_query.answer(results)
-
-
-
+def helpping(bot,update):
+    reply_message="你可以說的有\n\n{}".format(tags)
+    update.message.reply_text(reply_message)
  
