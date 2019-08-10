005a73854549c608cd064c2623d1c166224754ed
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-08-11 02:37:42 +0800

    Fix tags not define

--- app/modules/Yan/yan.py
+++ app/modules/Yan/yan.py
@@ -1,39 +1,45 @@
 import json
-import random, time
+import random
+import time
 from telegram.ext import InlineQueryHandler
-from telegram import InlineQueryResultArticle,InputTextMessageContent
+from telegram import InlineQueryResultArticle, InputTextMessageContent
 import logging
 import uuid
-YanDataPath='app/modules/Yan/yan.json'
-f=open(YanDataPath,encoding='utf-8')
-info=f.read()
-Data=dict(json.loads(info))['list']
-tags='['
+YanDataPath = 'app/modules/Yan/yan.json'
+f = open(YanDataPath, encoding='utf-8')
+info = f.read()
+Data = dict(json.loads(info))['list']
+tags = '['
+
 
 def return_all_emoji():
     yans = []
+    tags = ""
     for item in Data:
         tags = tags+"["+item["tag"]+"]"+'\n'
         yans.append(item['yan'])
     return yans
 
+
 def post_yan(bot, update):
     query = update.inline_query.query
     if len(query) == 0:
         return
-    results=list()
+    results = list()
     for i in Data:
         if query in i['tag']:
             for y in i['yan']:
                 results.append(
                     InlineQueryResultArticle(
-                    id=str(uuid.uuid4()),
-                    title=y,
-                    input_message_content=InputTextMessageContent(y)
+                        id=str(uuid.uuid4()),
+                        title=y,
+                        input_message_content=InputTextMessageContent(y)
                     )
                 )
     print(len(results))
     update.inline_query.answer(results)
-def helpping(bot,update):
-    reply_message="你可以說的有\n\n{}".format(tags)
+
+
+def helpping(bot, update):
+    reply_message = "你可以說的有\n\n{}".format(tags)
     update.message.reply_text(reply_message)
